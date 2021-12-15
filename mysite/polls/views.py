d94cfea8565ae796b7fda2db97cadab514b201c7
mink21 <minkyoung2112@gmail.com>
2021-12-16 01:17:55 +0900

    part 3

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponse, Http404
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello World. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    output = ', '.join([q.question_text for q in latest_question_list])
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question Does Not Exist")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    return HttpResponse("You're looking at the results of question %s." % question_id)
+
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    # return HttpResponse("You're voting on quetsion %s." % question_id)
